6806fa48a116ce37e95ee2fd7ab1890f1c8c17e5
Guilherme Fickel <guilhermefickel@gmail.com>
2024-11-08 12:40:28 -0300

    UPD: small code changes to test.py

--- test.py
+++ test.py
@@ -45,16 +45,8 @@ if __name__ == '__main__':
                     cfg.num_decoder_layers, cfg.vocab_size, cfg.max_sequence_length, dropout=cfg.dropout, emb_dropout=cfg.emb_dropout,
                     overlap=cfg.overlap)
 
-    # train_dataset = SyntheticOCRDataset(vocab, max_sequence_length, 50000)
-    # train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=4)
-
-    # val_dataset = SyntheticOCRDataset(vocab, max_sequence_length, batch_size*4)
-    # val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
     data_transform = transforms.Compose([
-        # transforms.ToPILImage(),
-        # transforms.RandAugment(num_ops=3, magnitude=7),
         transforms.ToTensor(),
-        transforms.Lambda(lambda img: img.float() / (255.0 if cfg.norm_image else 1.0)),
     ])
     
     state_dict = torch.load(args.model_path, map_location=device)
@@ -73,8 +65,6 @@ if __name__ == '__main__':
     dataset = ALPRDataset(
         f'../alpr_datasets/{test_ds}/alpr_annotation.csv',
         f'../alpr_datasets/{test_ds}/', #None)
-        # '../alpr_datasets/CCPD2019/alpr_annotation.csv',
-        # '../alpr_datasets/CCPD2019/ccpd_base/', #None)
         data_transform,
         return_image_path=True)
     dataloader = DataLoader(dataset, batch_size=1, shuffle=False, pin_memory=True, num_workers=6, drop_last=True)
@@ -82,7 +72,7 @@ if __name__ == '__main__':
     os.makedirs('out_images', exist_ok=True)
     ocr_missing, ocr_errors, ocr_correct = 0, 0, 0
     runtimes = []
-    for i, data in enumerate(dataloader):#enumerate(dummy_dataloader(5000, batch_size)):
+    for i, data in enumerate(dataloader):
         images, images_path, gt_ocr = data
         begin = time.time()
         ocrs_logits = model(images.to(DEVICE))
@@ -94,7 +84,6 @@ if __name__ == '__main__':
             if len(ocrs_pred[im_idx]) > 0:
                 if torch.equal(ocrs_pred[im_idx][1:], gt_ocr[im_idx][1:]):
                     ocr_correct += 1
-                    # print(f'Good: {ocrs_pred[im_idx]} | {gt_ocr[im_idx]}')
                 else:
                     ocr_errors += 1
                     ocr_text = indices_to_text(ocrs_pred[0].detach().cpu(), vocab)
@@ -105,8 +94,8 @@ if __name__ == '__main__':
                 ocr_missing += 1
 
         print(f'Acc: {ocr_correct/(ocr_correct+ocr_missing+ocr_errors)*100:.1f}% - {ocr_correct}|{ocr_correct+ocr_missing+ocr_errors}\t\t', end='\r')
-        if i == 500:
-            break
+        # if i == 500:
+        #     break
 
     print(f'Acc: {ocr_correct/(ocr_correct+ocr_missing+ocr_errors)*100:.1f}% - {ocr_correct}|{ocr_correct+ocr_missing+ocr_errors}\t\t')
     print(f'Runtime mean: {np.mean(runtimes)*1000:.2f}ms, std: {np.std(runtimes)*1000:.2}ms')
